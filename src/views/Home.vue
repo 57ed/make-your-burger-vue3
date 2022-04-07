a60d317f695ec59f617be213d31f4d928e769b0b
57ed <edugitestboia@gmail.com>
2022-04-07 06:51:25 -0300

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <Banner />
+  <div class="main-container">
+    <h1>Monte o seu burger:</h1>
+    <BurgerForm />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Banner from "@/components/Banner.vue";
+import BurgerForm from "@/components/BurgerForm";
+
+export default {
+  name: "Home",
+  components: {
+    Banner,
+    BurgerForm,
+  },
+};
+</script>
